a1fa6fab1f4e16049cded9c42ae8cea13c2aa958
Somya Jha <sumicode.exe@gmail.com>
2023-07-28 16:12:21 +0530

    blur

---
+++ Introduction/marks.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int m;
+
+    printf("Enter marks: ");
+    scanf("%d", &m);
+
+    if (m > 75){
+        printf("Outstanding");
+    } else if ((m >= 60) && (m < 75)){
+        printf("Amazing");
+    } else if ((m >= 50) && (m < 60)){
+        printf("Boomer");
+    } else if ((m >= 40) && (m < 50)){
+        printf("Cunt");
+    } else {
+        printf("Dont study");
+    }
+
+    return 0;
+}
